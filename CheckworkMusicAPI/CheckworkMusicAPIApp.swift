04cea7684b2c4fecfa23f86616dd82ede8f4e0ae
moyotsukai <gyoshu.bowie@gmail.com>
2024-06-12 14:38:32 +0900

    Initial commit

---
+++ CheckworkMusicAPI/CheckworkMusicAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckworkMusicAPIApp.swift
+//  CheckworkMusicAPI
+//
+//  Created by Owner on 2024/06/12.
+//
+
+import SwiftUI
+
+@main
+struct CheckworkMusicAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
